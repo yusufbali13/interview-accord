32d5143be4f7cd70ee98f6fdadac3c1e86d0a8a1
Yusuf <baliyusuf675@gmail.com>
2023-02-25 18:01:54 +0300

    first commit

---
+++ src/components/InterviewAccord.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Question from './Question';
+
+const InterviewAccord = (props) => {
+    const {data} = props;
+    console.log('data',props);
+    
+  return (
+    <div className='row'>
+        {
+           data.map(item => <Question key={item.id} {...item} />)
+        }
+    </div>
+  )
+}
+
+export default InterviewAccord
